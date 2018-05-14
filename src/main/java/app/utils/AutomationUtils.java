70d029134271d2f22da93bc07095b18d774ac2b0
Prasanna Dommalapati <dommalapati.chowdary@gmail.com>
2018-05-14 14:10:17 +0100

    browser twaeks

--- src/main/java/app/utils/AutomationUtils.java
+++ src/main/java/app/utils/AutomationUtils.java
@@ -6,20 +6,63 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 
 public class AutomationUtils
 {
 	private static String baseUrl = "https://www.google.co.uk";//keep ur own base URL
-	private static String OS = null; 
+	public static final String BROWSER = "chrome";
+	public static WebDriver driver = null;
 	
 	public static WebDriver getWebDriver() throws IOException
 	{
+				
+		switch (BROWSER) {
+		case "chrome":
+			if(getOS().equals("Mac OS X")) 
+				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/assets/mac/chromedriver");
+			 else
+				 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\assets\\windows\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			if(getOS().equals("Mac OS X")) 
+				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/assets/mac/geckodriver");
+			 else
+				 System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\assets\\windows\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		case "ie":
+			if(getOS().equals("Mac OS X")) 
+				System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"/assets/mac/iedriver");
+			 else
+				 System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\assets\\windows\\iedriver.exe");
+			driver = new InternetExplorerDriver();
+			break;
+		case "edge":
+			if(getOS().equals("Mac OS X")) 
+				System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"/assets/mac/edgedriver");
+			 else
+				 System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\assets\\windows\\edgedriver.exe");
+			driver = new EdgeDriver();
+			break;
+		case "safari":
+			if(getOS().equals("Mac OS X")) 
+				System.setProperty("webdriver.safari.driver", System.getProperty("user.dir")+"/assets/mac/safaridriver");
+			 else
+				 System.setProperty("webdriver.safari.driver", System.getProperty("user.dir")+"\\assets\\windows\\safaridriver.exe");
+			driver = new SafariDriver();
+			break;
+
+		default:
+			System.out.println("no such browser");
+			break;
+		}
 		
-		setDriverPath();
-		//System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\assets\\windows\\geckodriver.exe");
-		
-		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get(baseUrl);
 		
@@ -28,22 +71,12 @@ public class AutomationUtils
 	
 	public static String getOS() {
 		
-		OS = System.getProperty("os.name");
-		
-		return OS;
-	}
-	
-	public static void setDriverPath() {
-		if(getOS().equals("Mac OS X")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/assets/mac/chromedriver"); 
-		} else {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\assets\\windows\\chromedriver.exe");
-		}
+		return System.getProperty("os.name");
 	}
 	
 	public static String getDataLocation() {
 		if(getOS() == null) {
-			return "";
+			return "You are running Tests in invalid Operating System! Please try in valid operating System";
 		}
 		else if(getOS().equals("Mac OS X")) {
 			return System.getProperty("user.dir")+"/assets/data/InputDataVersionPositive1.0.xlsx";
